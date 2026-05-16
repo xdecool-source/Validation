c519cb7b3ec22d15e02b272d574a2a9db681fcaa
xdo <xavier.decool@5asolutions.com>
2026-05-16 11:40:49 +0200

    config.py date

--- app/config.py
+++ app/config.py
@@ -14,7 +14,7 @@ temps_expi_token = 5
 class Settings:
     DATABASE_URL: str = os.getenv("DATABASE_URL")  
     MATCH_DAYS = [
-        {"id": 1, "code": "J1", "date": "2026-05-17"},
+        {"id": 1, "code": "J1", "date": "2026-09-20"},
         {"id": 2, "code": "J2", "date": "2026-10-04"},
         {"id": 3, "code": "J3", "date": "2026-10-18"},
         {"id": 4, "code": "J4", "date": "2026-11-08"},
