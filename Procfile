7700b3cc6c5de7b1978db9faa2bfc75ba76df0ee
xdo <xavier.decool@5asolutions.com>
2026-08-26 10:21:25 +0200

    update

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: uvicorn app.validate:app --host 0.0.0.0 --port $PORT
--- app/database.py
+++ app/database.py
@@ -11,8 +11,16 @@ from app.config import settings
 
 load_dotenv()  # charge le .env
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL n'est pas définie dans le fichier .env")
+
 if DATABASE_URL.startswith("postgres://"):
-    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
-    
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgres://",
+        "postgresql://",
+        1,
+    )
+
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(bind=engine)
